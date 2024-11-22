f062a92663c57f37c73eebbe7aefd25cf27f5b66
Archonic <gabrielc2904@gmail.com>
2024-11-22 16:31:00 -0500

    Scaffolding for Time

--- source/c11e1/Time.cpp
+++ source/c11e1/Time.cpp
@@ -4,14 +4,20 @@
 
 using namespace std;
 
-Time::Time(int s){
-
+Time::Time(int ss){
+    s = ss;
 }
 
 Time::Time(){
-
+    s = 0;
 }
 
 string Time::to_string(){
-    return "";
+    int m = s/60;
+    int h = m/60;
+    string s_str = std::to_string(s%60);
+    if(s_str.length() == 1) s_str = "0" + s_str;
+    string m_str = std::to_string(m%60);
+    if(m_str.length() == 1) m_str = "0" + m_str;
+    return std::to_string(h) + ":" + m_str + ":" + s_str;
 }
