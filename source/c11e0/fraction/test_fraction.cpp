1dc8d3e08b1e2cfd02244bbecc8fc66e6f226c5f
Archonic <gabrielc2904@gmail.com>
2024-12-13 11:44:52 -0500

    Add integer to_string test

--- source/c11e0/fraction/test_fraction.cpp
+++ source/c11e0/fraction/test_fraction.cpp
@@ -81,3 +81,10 @@ TEST_CASE("Test can construct Fraction from integer") {
     CHECK(f2.num == 42);
     CHECK(f2.den == 1);
 }
+
+TEST_CASE("Test integer Fractions render properly") {
+    Fraction f1(5, 1);
+    CHECK(f1.to_string() == "5");
+    Fraction f2(18, 3);
+    CHECK(f2.to_string() == "6");
+}
