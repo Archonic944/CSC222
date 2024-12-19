062e127b199b20a69404447accce7fef6ab961fc
Archonic <gabrielc2904@gmail.com>
2024-12-18 20:43:11 -0500

    3rd test + some organization

--- source/c12/e0/Cards.h
+++ source/c12/e0/Cards.h
@@ -6,6 +6,7 @@ enum Suit { NONE, CLUBS, DIAMONDS, HEARTS, SPADES };
 enum Rank { JOKER, TWO = 2, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, 
             NINE, TEN, JACK, QUEEN, KING, ACE };
 
+//Single card structure
 struct Card
 {
     Suit suit;
@@ -19,4 +20,19 @@ struct Card
     bool operator<=(const Card&) const;
     bool operator!=(const Card&) const;
     string to_string() const;
+};
+
+// Deck structure
+struct Deck {
+    vector<Card> cards;
+
+    Deck(int size);
+    Deck();
+    void print() const;
+    void shuffle();
+    void sort();
+    Deck subdeck(int l, int h) const;
+    void add_card(const Card& c);
+    Card remove_card();
+    void swap_cards(int i, int j);
 };
--- source/c12/e0/Deck.cpp
+++ source/c12/e0/Deck.cpp
@@ -1,72 +1,72 @@
-#include <iostream>
 #include <string>
+#include <iostream>
 #include "Cards.h"
 #include <vector>
 
-struct Deck {
-    vector<Card> cards;
+//utils
+int random_between(int low, int high) {
+    return 0;
+}
 
-    Deck(int size) {
-        vector<Card> temp(size);
-        cards = temp;
+void Deck::print() const {
+    for (int i = 0; i < cards.size(); i++) {
+        cout << cards[i].to_string() << endl;
     }
+}
 
-    Deck() {
-        vector<Card> temp(52);
-        cards = temp;
-        int i = 0;
-        for (Suit suit = CLUBS; suit <= SPADES; suit = Suit(suit + 1)) {
-            for (Rank rank = TWO; rank <= ACE; rank = Rank(rank + 1)) {
-                cards[i].suit = suit;
-                cards[i].rank = rank;
-                i++;
-            }
+//constructors
+Deck::Deck(int size) {
+    vector<Card> temp(size);
+    cards = temp;
+}
+
+Deck::Deck() {
+    vector<Card> temp(52);
+    cards = temp;
+    int i = 0;
+    for (Suit suit = CLUBS; suit <= SPADES; suit = Suit(suit + 1)) {
+        for (Rank rank = TWO; rank <= ACE; rank = Rank(rank + 1)) {
+            cards[i].suit = suit;
+            cards[i].rank = rank;
+            i++;
         }
     }
+}
 
-    void print() const {
-        for (int i = 0; i < cards.size(); i++) {
-            cout << cards[i].to_string() << endl;
-        }
+//mutators
+void Deck::shuffle() {
+    for (int i = 0; i < cards.size(); i++) {
+        int rand_card = random_between(0, cards.size() - 1);
+        swap_cards(i, rand_card);
     }
+}
 
-    void shuffle() {
-        for (int i = 0; i < cards.size(); i++) {
-            int rand_card = random_between(0, cards.size() - 1);
-            swap_cards(i, rand_card);
-        }
+void Deck::sort() {
+    for (int i = 0; i < cards.size(); i++) {
+        //find the lowest card at or to the right of i
+        //swwap the ith card and the lowest card
     }
+}
 
-    void sort() {
-        for (int i = 0; i < cards.size(); i++) {
-            //find the lowest card at or to the right of i
-            //swwap the ith card and the lowest card
-        }
+//add/change contents
+Deck Deck::subdeck(int l, int h) const {
+    Deck sub(h - l + 1);
+    for (int i = 0; i < sub.cards.size(); i++) {
+        sub.cards[i] = cards[l + i];
     }
+    return sub;
+}
 
-    Deck subdeck(int l, int h) const {
-        Deck sub(h - l + 1);
-        for (int i = 0; i < sub.cards.size(); i++) {
-            sub.cards[i] = cards[l + i];
-        }
-        return sub;
-    }
+void Deck::add_card(const Card& c) {
+    cards.push_back(c);
+}
 
-    void add_card(const Card& c) {
-        cards.push_back(c);
-    }
+Card Deck::remove_card() {
+    Card card = cards[cards.size() - 1];
+    cards.pop_back();
+    return card;
+}
 
-    Card remove_card() {
-        Card card = cards[cards.size() - 1];
-        cards.pop_back();
-        return card;
-    }
-
-    int random_between(int low, int high) {
-        return 0;
-    }
-
-    void swap_cards(int i, int j) {
-        
-    }
-};
+void Deck::swap_cards(int i, int j) {
+    
+}
